c5e8c00619cd498c308f1f93c8614595cff43b43
ultimatealik <ultimatealik@gmail.com>
2016-12-25 21:44:05 +0700

    sin
    1st commit

---
+++ Sinus.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <math.h>
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+#define PI 3.14159265 
+
+int main()
+{
+	float x = 0, y = 0;
+	cout << "Введите x: ";
+	cin >> x;
+	y = sin(x * PI / 180);
+	cout << "y = " << y;
+	getch();
+}
